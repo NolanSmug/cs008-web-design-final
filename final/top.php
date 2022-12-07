3f83cb83079f66a5a96fc46afb9ad22c4f625d50
Nolan <112602295+NolanSmug@users.noreply.github.com>
2022-12-07 10:33:16 -0500

    Final project: top.php

--- final/top.php
+++ final/top.php
@@ -10,7 +10,8 @@ $pathParts = pathinfo($phpSelf);
         <meta charset="utf-8">
         <title>Music Around the World</title>
         <meta name="author" content="Nolan Cyr and Matt Swaim">
-        <meta name="description" content=" "> <!-- Fill description after -->
+        <meta name="description" content="All about music, bands, and guitar. Learn how to play the guitar, learn about the history of the guitar, 
+        and learn about famous bands that incorporate the guitar">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     
         <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
